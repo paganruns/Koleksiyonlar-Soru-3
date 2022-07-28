dbc1c486ab275b1430e06f8bb65c8540c14b6a53
SahinZambak <sahinzambak7@gmail.com>
2022-07-28 17:15:32 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir Cümle Giriniz.");
+        var cumle = Console.ReadLine();
+
+        int a = 0;
+
+        string[] sesli = new string[cumle.Length];
+
+        for (int i = 0; i < cumle.Length; i++)
+        {
+            if ("aeiouöüıAEIOUÖÜİ".IndexOf(cumle[i]) >= 0)
+            {
+                sesli[a] = new string(cumle[i], 1);
+                a++; // sesli harf sayısı
+            }
+
+        }
+
+        Array.Resize(ref sesli, a); // sesli harf sayısının düzeltilmesi
+
+        foreach (var item in sesli)
+        {
+            System.Console.WriteLine(item);
+        }
+        
+
+
+    }
+}
